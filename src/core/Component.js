4aa63ddff25fc6164494ba1683c116ffc4e2176a
leekyoungmin121 <kmlee@hdtel.co.kr>
2022-02-09 09:30:35 +0900

    이벤트 추상화

--- src/core/Component.js
+++ src/core/Component.js
@@ -5,6 +5,7 @@ export default class Component {
     this.$target = $target;
     this.setup();
     this.render();
+    this.setEvent();
   }
   setup() {}
   template() {
@@ -12,12 +13,24 @@ export default class Component {
   }
   render() {
     this.$target.innerHTML = this.template();
-    this.setEvent();
   }
-  
+
   setEvent() {}
   setState(newState) {
     this.$state = { ...this.$state, ...newState };
     this.render();
+  }
+
+  addEvent(eventType, selector, callback) {
+    const children = [...this.$target.querySelectorAll(selector)];
+
+    // selector 에 명시한 것 보다 더 하위 요소가 선택되는 경우가 있을 땐
+    // closest 를 이용하여 처리
+    const isTarget = (target) => children.includes(target) || target.closest(selector);
+
+    this.$target.addEventListener(eventType, (event) => {
+      if (!isTarget(event.target)) return false;
+      callback(event);
+    });
   }
 }
